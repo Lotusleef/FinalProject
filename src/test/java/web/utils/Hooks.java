1890bf0f88de3a4dc92549666dc9f3e8013a7e4f
Muhammad Rayhan <muhdryhn@gmail.com>
2025-10-06 04:43:35 +0300

    fix on web/utils/Hooks.java

--- src/test/java/web/utils/Hooks.java
+++ src/test/java/web/utils/Hooks.java
@@ -10,17 +10,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Hooks {
     public static WebDriver driver;
 
-    // web/utils/Hooks.java
     @Before("@web")
     public void setUp() {
         io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
+
+
+        boolean headless = System.getenv("CI") != null || Boolean.getBoolean("headless");
+        if (headless) {
+            options.addArguments("--headless=new", "--disable-gpu",
+                    "--no-sandbox", "--disable-dev-shm-usage", "--window-size=1920,1080");
+
+            String chromeBin = System.getenv("CHROME_PATH");
+            if (chromeBin != null && !chromeBin.isBlank()) {
+                options.setBinary(chromeBin);
+            }
+        }
+
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-
         driver.manage().timeouts().pageLoadTimeout(java.time.Duration.ofSeconds(30));
-        driver.manage().timeouts().implicitlyWait(java.time.Duration.ZERO); // <-- matikan implicit
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ZERO); // tetap 0
     }
 
 
